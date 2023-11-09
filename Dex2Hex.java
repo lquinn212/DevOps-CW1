5ecffb674a1f841f8456406c51e2301501e4fc87
“lquinn212” <lquinn212@caledonian.ac.uk>
2023-11-09 16:01:02 +0000

    Adding exemption catch at beginning of code to avoid loop

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -6,8 +6,11 @@ class Dec2Hex
     public static int Arg1;
 
     public static void main(String args[])
-    {
-        Arg1 = Integer.parseInt(args[0]);
+    { try{
+        Arg1 = Integer.parseInt(args[0]);}
+
+	catch (NumberFormatException e) {System.out.println("Please insert a decimal value");}
+
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
