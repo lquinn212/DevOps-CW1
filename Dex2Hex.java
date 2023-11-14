507aff5f322d41f44902454818d44da354909629
“lquinn212” <lquinn212@caledonian.ac.uk>
2023-11-14 22:53:25 +0000

    Added try and catch with scanner again but changed to more generic exception

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -6,7 +6,19 @@ class Dec2Hex
     public static int Arg1;
 
     public static void main(String args[])
-    {
+    {	
+	{
+        Scanner scanner = new Scanner(System.in);
+        int inputDecimal;
+
+        try {
+            inputDecimal = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter a decimal value.");
+            scanner.nextLine();
+            inputDecimal = scanner.nextInt();
+        }
+
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
