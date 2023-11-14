50931f47281bdf18602bf3e8d8abb4bf15fedbf8
“lquinn212” <lquinn212@caledonian.ac.uk>
2023-11-14 20:39:12 +0000

    Infinite loop still occuring so reverting to orignal code

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -6,10 +6,7 @@ class Dec2Hex
     public static int Arg1;
 
     public static void main(String args[])
-    {	
-    
-    try{
-    try {
+    {
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
@@ -25,12 +22,8 @@ class Dec2Hex
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
-        
-        } catch (Error e) {
-      System.out.println("Something went wrong.");}}
-      
-      	catch (Exception e) {
-      System.out.println("Something went wrong.");}
-      
-}}
+
+    }
+}
+
 
