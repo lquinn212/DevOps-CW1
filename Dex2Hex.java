864c1cbae18256720f36e15547e3e059cd187256
“lquinn212” <lquinn212@caledonian.ac.uk>
2023-11-14 20:16:55 +0000

    Added catch exception AND catch error to try stop infinite looping

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -6,7 +6,10 @@ class Dec2Hex
     public static int Arg1;
 
     public static void main(String args[])
-    {
+    {	
+    
+    try{
+    try {
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
@@ -22,7 +25,12 @@ class Dec2Hex
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
-
-    }
-}
+        
+        } catch (Error e) {
+      System.out.println("Something went wrong.");}}
+      
+      	catch (Exception e) {
+      System.out.println("Something went wrong.");}
+      
+}}
 
