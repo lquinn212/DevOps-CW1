552cb41e9f325d53d1fc2b18b0726fa42432eb71
“lquinn212” <lquinn212@caledonian.ac.uk>
2023-11-14 22:44:17 +0000

    try and catch with scanner still crashed instance. Reverting to original code

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,4 +1,3 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Dec2Hex
@@ -8,25 +7,12 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        Scanner scanner = new Scanner(System.in);
-        int inputDecimal;
-
-        try {
-            inputDecimal = scanner.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("Your input is invalid. Please enter a decimal value.");
-            scanner.nextLine();
-            inputDecimal = scanner.nextInt();
-        }
-
-
-        Arg1 = Integer.parseInt(args[inputDecimal]);
+        Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
 
         while(num != 0)
         {
@@ -37,6 +23,8 @@ class Dec2Hex
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
 
-} }
+    }
+}
+
 
 
