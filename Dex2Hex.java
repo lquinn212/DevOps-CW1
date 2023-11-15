008bdb506b2bc5d882a5a5f3308352a2fdcc8e69
“lquinn212” <lquinn212@caledonian.ac.uk>
2023-11-15 15:39:38 +0000

    Added square calculation to check if simple calcs crash instance as well as the try and catch

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 class Dec2Hex
 {
-
     public static int Arg1;
 
     public static void main(String args[])
@@ -11,17 +10,20 @@ class Dec2Hex
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
+        int num2= Arg1;
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        while(num != 0)
+        while(num != 0 && num2 !=0)
         {
             rem=num%16;
             hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
+            num2 = num2*num2;
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
+        System.out.println("Your chosen value squared is: " + num2);
 
     }
 }
